c4b42a6d1d6bbdbd1bb4e1287d56dca6f28d3f5a
Lalith <lalithr95@gmail.com>
2016-02-07 21:31:57 +0530

    Add font asset

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,7 @@ module Invoice
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.serve_static_files = true
+    config.assets.paths << "#{Rails.root}/app/assets/fonts"
 
     config.active_record.raise_in_transactional_callbacks = true
   end
