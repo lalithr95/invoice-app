ca7d1278ac186adb23e24d6a088dd476c8a1e12b
Lalith <lalithr95@gmail.com>
2016-02-07 21:38:50 +0530

    add assets precompile

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,7 @@ module Invoice
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.serve_static_files = true
     config.assets.paths << "#{Rails.root}/app/assets/fonts"
-
+    config.assets.compile = true
     config.active_record.raise_in_transactional_callbacks = true
   end
 end
