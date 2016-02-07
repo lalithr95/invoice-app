25ecb4068bf83d21ddf613f7238214ca8d19c494
Lalith <lalithr95@gmail.com>
2016-02-08 03:42:34 +0530

    backend for authentication and basic functionality

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,9 @@
 class DashboardController < ApplicationController
-  before_filter :authenticate_user!
 	def index
+    if user_signed_in?
+      render 'index'
+    else
+      render temlate: 'dashboard/auth'
+    end
 	end
 end
