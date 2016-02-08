3af754f3f74b65d7f550aea0e344f5843b8ace58
Lalith <lalithr95@gmail.com>
2016-02-08 17:47:07 +0530

    improvements to auth flow

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,4 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
-  private
-  def sign_up_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
-  end
-
-  def account_update_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :current_password)
-  end
 end
