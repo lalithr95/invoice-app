341dbd8367af0fc5569faf312a354132b9983257
Lalith <lalithr95@gmail.com>
2016-02-27 15:09:19 +0530

    rspec and capybara added

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,10 @@ module Invoice
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-
+    config.generators do |g|
+       g.test_framework  :rspec
+       g.integration_tool :rspec
+    end
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.serve_static_files = true
     config.assets.paths << "#{Rails.root}/app/assets/fonts"
